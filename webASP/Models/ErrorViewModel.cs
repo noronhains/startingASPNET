5549188e8064ee8d905b963116ac27a29038645e
Igor Noronha <igoru.noronha@gmail.com>
2018-06-16 13:05:14 -0300

    Adicionado projeto MVC.

---
+++ webASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
